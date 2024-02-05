902931c70e9ca6e126e232e6a42723e7db76a589
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-02-05 17:16:20 -0600

    adding validation

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -33,13 +33,17 @@ namespace ToDoList.Controllers
     [HttpPost]
     public ActionResult Create(Item item)
     {
-      if (item.CategoryId == 0)
+      if (!ModelState.IsValid)
       {
-        return RedirectToAction("Create");
+          ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+          return View(item);
       }
-      _db.Items.Add(item);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+      else
+      {
+        _db.Items.Add(item);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
     }
 
     public ActionResult Details(int id)
@@ -81,7 +85,8 @@ namespace ToDoList.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-      public ActionResult AddTag(int id)
+
+    public ActionResult AddTag(int id)
     {
       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
       ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Title");
@@ -100,14 +105,15 @@ namespace ToDoList.Controllers
         _db.SaveChanges();
       }
       return RedirectToAction("Details", new { id = item.ItemId });
-    }  
-     [HttpPost]
+    }   
+
+    [HttpPost]
     public ActionResult DeleteJoin(int joinId)
     {
       ItemTag joinEntry = _db.ItemTags.FirstOrDefault(entry => entry.ItemTagId == joinId);
       _db.ItemTags.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
-    }
+    } 
   }
 }
